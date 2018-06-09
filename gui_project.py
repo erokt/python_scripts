0ea903b0d737e3e47de41bfb39c977e80e1e255d
erokt <erokt5.foe@gmail.com>
2018-06-08 21:58:57 -0500

    Changing the spacing and removing import urllib

--- gui_project.py
+++ gui_project.py
@@ -5,10 +5,11 @@ import Tkinter
 import tkMessageBox
 import os
 import sys
-import urllib
+
 
 top = Tkinter.Tk()
 top.title("PyKeys")
+
 
 #Creating my message box data string.
 data = """
@@ -16,6 +17,8 @@ Hello Everyone\n\nWelcome to our first venture into creating a GUI!!!
 \n\nIf you want to know how I did this, go to this website:\n\n
 https://www.tutorialspoint.com/python/python_gui_programming.htm
 """
+
+
 #define the following.
 def aboutCallBack():
 	tkMessageBox.showinfo( "About", '%s' % data)
@@ -47,6 +50,8 @@ def rtopsidCallBack():
 def rtopimpCallBack():
 	os.system('/home/usrname/Documents/ShortKeys/ttimpact.py')
 	print "Done...But keep terminal open!"
+
+
 	#Dis what it's gon say at the beginning.
 B = Tkinter.Button(top, text ="About this Script", command = aboutCallBack, foreground="red")
 C = Tkinter.Button(top, text="Vendor Update", command = vupdateCallBack, foreground="blue")
@@ -66,5 +71,7 @@ F.pack()
 G.pack()
 H.pack()
 I.pack()
+
+
 #you have to leave this here, so that it will always run...it's a loop
 top.mainloop()
